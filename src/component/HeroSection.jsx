93eee4fbb0f59f01c9b60cf7f33a262854acd2fb
JazzyRain14 <ajadiolamilekan14@gmail.com>
2024-10-09 08:46:18 +0100

    finished project update

--- src/component/HeroSection.jsx
+++ src/component/HeroSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const HeroSection = () => {
     return (
-        <div className='bg-hero-pattern w-full h-[600px] px-[20px] bg-cover bg-no-repeat max-sm:bg-left bg-center flex flex-col justify-end items-center border border-black pb-6'>
+        <div className='bg-hero-pattern w-full h-[600px] px-[20px] bg-cover bg-no-repeat max-sm:bg-left bg-center flex flex-col justify-end items-center pb-6'>
             <div className='grid place-items-center'>
                 <h1 className='w-full font-SourceBold text-3xl leading-8 text-white text-center font-bold'>Lorem ipsum dolor set amet lorem ipsum</h1>
                 <p className='text-white mt-1 font-SourceBold text-center'>Lorem ipsum dolor set amet lorem ipsum dolor set</p>
